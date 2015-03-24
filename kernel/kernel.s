0501fbddfee03ef5a51d05d629eb4c86701f5328
mariashka <bisyarinamariashka@gmail.com>
2015-03-24 23:12:22 +0300

    Add stack

--- kernel/kernel.s
+++ kernel/kernel.s
@@ -6,9 +6,21 @@ global kernel_main
 
 ;;; Entry point of the kernel.
 kernel_main:
+	mov esp, stack_top
+	mov byte al, 1
+	push eax
+	mov byte al, 2
+	TTY_PUTC al
+	pop eax
+	TTY_PUTC al
         TTY_PUTS_STYLED TTY_STYLE(TTY_RED, TTY_BLUE), hello_world
 	hlt
 	jmp $
 
 section .data
 hello_world: db 'Hello, world!', 0
+
+section .bss
+stack:
+        resd 0x1000
+stack_top:  
