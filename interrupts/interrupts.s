5d8b0dcc15e378d5f43b093f3f923c9c9a3b49a8
mariashka <bisyarinamariashka@gmail.com>
2015-03-24 23:49:22 +0300

    Add empty IDT

--- interrupts/interrupts.s
+++ interrupts/interrupts.s
@@ -1,10 +1,16 @@
 section .text
 
 global init_interrupts
-init_interrupts:
 
+INTERRUPTS_NUMBER	equ	1 << 11
+
+
+init_interrupts:
+	lidt [interrupt_table]
+
+	sti
+	iret
 
 section .data
 interrupt_table:
-
-.ptr:
+	times INTERRUPTS_NUMBER dd 0
