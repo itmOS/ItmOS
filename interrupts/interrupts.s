05740685c9419a239cf659ad04446393cbbbbed4
mariashka <bisyarinamariashka@gmail.com>
2015-03-31 23:31:59 +0300

    Add mapping IRQ, loading IDT and IRQ0 handling

--- interrupts/interrupts.s
+++ interrupts/interrupts.s
@@ -2,15 +2,64 @@ section .text
 
 global init_interrupts
 
-INTERRUPTS_NUMBER	equ	1 << 11
+INTERRUPTS_TABLE_SIZE   equ	1 << 11
+IRQ_BASE                equ	0x20
 
+%include "tty/tty.inc"
+
+timer_int_handler:
+        pusha
+        xchg bx, bx
+        popa
+        iret
+        
+init_timer_int_handler:
+        xchg bx, bx
+        pusha
+
+        mov eax, timer_int_handler
+        mov [interrupt_table + IRQ_BASE * 8], ax
+        mov word [interrupt_table + IRQ_BASE * 8 + 2], 8
+        mov word [interrupt_table + IRQ_BASE * 8 + 4], 0x8E00
+        shr eax, 16
+        mov [interrupt_table + IRQ_BASE * 8 + 6], ax
+
+        popa
+        ret
 
 init_interrupts:
-	lidt [interrupt_table]
+        push eax
+        xchg bx, bx
+	lidt [interrupt_table.ptr]
+        
+                 mov al, 0x11
+         out 0x20, al
+         mov al, 8
+         out 0x21, al
+         mov al, 4
+         out 0x21, al
+         mov al, 1
+         out 0x21, al
+        
 
-	sti
-	iret
+         mov al, 0x11
+         out 0xA0, al
+         mov al, 0x20
+         add al, 8
+         out 0xA1, al
+         mov al, 2
+         out 0xA1, al
+         mov al, 1
+         out 0xA1, al
 
-section .data
+        call init_timer_int_handler
+        pop eax
+        sti
+	ret
+
+;;; section .data
 interrupt_table:
-	times INTERRUPTS_NUMBER dd 0
+	times INTERRUPTS_TABLE_SIZE dd 0
+.ptr:
+        dw INTERRUPTS_TABLE_SIZE
+        dd interrupt_table
