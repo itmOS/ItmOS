b43956a9f5ddec0087b32f87243d965a4aeae6cd
itegulov <wibkwibk@gmail.com>
2015-04-05 19:52:28 +0300

    Implemented read_byte and write_byte

--- ata/ata.s
+++ ata/ata.s
@@ -1,11 +1,60 @@
-global ata_read
+global ata_pio_inbyte
+global ata_pio_outbyte
+
+;;; Shitty implementation of ATA driver. All errors (CRC and other) are ignored
+;;; at this moment.
 
 section .text
-;;; esi -- driver data info
-;;; edi -- destionation buffer
-;;; bl  -- sectors to read
-;;; dx  -- base bus IO port (probably 0x1F0)
-;;; ebp -- 28bit relative LBA
-ata_read:
-	;;; TODO implement
+;;; Reads byte from specified address. (Uses pio_base_addr as device)
+;;; Input:
+;;;		cx -- drive-begin relative address
+;;; Output:
+;;;		al -- result of reading from specified address
+ata_pio_inbyte:
+	push edx
+
+	mov dx, [pio_base_addr]
+	add dx, cx
+
+	in al, dx
+
+	pop edx
 	ret
+
+;;; Writes byte to specified address. (Uses pio_base_addr as device)
+;;; Input:
+;;;		cx -- drive-begin relative address
+;;;		al -- byte to write
+ata_pio_outbyte:
+	push edx
+
+	mov dx, [pio_base_addr]
+	add dx, cx
+
+	out dx, al
+
+	pop edx
+	ret
+
+
+
+section .data
+;;; Primary bus I/O port. Other ports are specified as
+;;; offset from this one.
+pio_base_addr: dd 0x1F0
+;;; 9 I/O ports, which control ATA bus behaviour.
+;;; For the primary bus, these I/O ports are 0x1F0
+;;; through 0x1F9.
+cb_data:  db 0 ;;; data reg      in/out
+cb_err:	  db 1 ;;; error         in
+cb_fr:    db 1 ;;; feature reg      out
+cb_sc:    db 2 ;;; sector count  in/out
+cb_sn:    db 3 ;;; sector number in/out
+cb_cl:    db 4 ;;; cylinder low  in/out
+cb_ch:    db 5 ;;; cylinder high in/out
+cb_dh:    db 6 ;;; device head   in/out
+cb_stat:  db 7 ;;; prim status   in
+cb_cmd:   db 7 ;;; command          out
+cb_astat: db 8 ;;; alt status    in
+cb_dc:    db 8 ;;; device ctrl      out
+cb_da:    db 9 ;;; device addr   in
--- kernel/kernel.s
+++ kernel/kernel.s
@@ -1,6 +1,7 @@
 section .text
 
 %include "tty/tty.inc"
+%include "ata/ata.inc"
 
 global kernel_main
 
@@ -8,12 +9,14 @@ global kernel_main
 kernel_main:
 	mov esp, stack_top
 	mov byte al, 1
-	push eax
-	mov byte al, 2
-	TTY_PUTC al
-	pop eax
-	TTY_PUTC al
-        TTY_PUTS_STYLED TTY_STYLE(TTY_RED, TTY_BLUE), hello_world
+	;push eax
+	;mov byte al, 2
+	;TTY_PUTC al
+	;pop eax
+	;TTY_PUTC al
+  ;      TTY_PUTS_STYLED TTY_STYLE(TTY_RED, TTY_BLUE), hello_world
+	ATA_PIO_OUTBYTE 100, 1
+	ATA_PIO_INBYTE 100
 	hlt
 	jmp $
 
@@ -23,4 +26,4 @@ hello_world: db 'Hello, world!', 0
 section .bss
 stack:
         resd 0x1000
-stack_top:  
+stack_top:
