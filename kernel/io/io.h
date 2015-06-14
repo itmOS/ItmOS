a6729a573e958f953244790b9f07aa08ee20f365
Zakhar Voit <zakharvoit@gmail.com>
2015-06-14 20:18:39 +0300

    Fix io.h

--- kernel/io/io.h
+++ kernel/io/io.h
@@ -1,6 +1,8 @@
 #ifndef KERNEL_IO_IO_H
 #define KERNEL_IO_IO_H
 
+#include "sys/types.h"
+
 // read/write/close functions should be asynchronious and return this
 // counstant if they would block
 #define IO_WOULD_BLOCK -(1 << 1)
@@ -8,8 +10,24 @@
 #define IO_WRITE (1 << 2)
 #define IO_CLOSE (1 << 3)
 
-typedef void fd_obj;
+// Struct representing some opened file descriptor info
+// Pointer to this structure can be shared between several processes
+typedef struct {
+  // Owners counter (like std::shared_ptr)
+  int counter;
+
+  // Pointers to the io methods.
+  int (*read)(struct fd_obj* this, void* buf, size_t count);
+
+  int (*write)(struct fd_obj* this, void* buf, size_t count);
+
+  int (*close)(struct fd_obj* this);
+
+  // All the rest is the object's private data
+  char data[];
+} fd_obj;
+
 // events is logical or of the IO_* macros
-void io_notify_available(int events);
+void io_notify_available(fd_obj* obj, int events);
 
 #endif
