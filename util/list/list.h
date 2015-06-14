6eda2fcda112b63109434b7d3efb8daba04c2771
Zakhar Voit <zakharvoit@gmail.com>
2015-06-14 18:09:47 +0300

    Add dynamic memory for lists

--- util/list/list.h
+++ util/list/list.h
@@ -5,9 +5,12 @@ typedef void* list;
 
 list* list_single(void*);
 void* list_head(list*);
+// Get tail without freeing the list
 list* list_tail(list*);
+// Free all list
 void list_free(list*);
 list* list_push(list*, void*);
+// Get tail and free the current head
 list* list_pop(list*);
 
 #endif
