7a963aa5d738105523467d93cd887a5ba639cc54
Zakhar Voit <zakharvoit@gmail.com>
2015-06-14 22:52:38 +0300

    Add the pipe_obj implementation (waiting for free fix)

--- kernel/io/pipe.h
+++ kernel/io/pipe.h
@@ -3,7 +3,28 @@
 
 #include <sys/types.h>
 
+#include "kernel/io/io.h"
+
 typedef void pipe_t;
+
+#define PIPE_READER 1
+#define PIPE_WRITER 2
+
+typedef struct pipe_obj_t
+{
+  unsigned int count;
+
+  int (*read)(struct pipe_obj_t* this, void* buf, size_t count);
+  int (*write)(struct pipe_obj_t* this, void* buf, size_t count);
+  int (*close)(struct pipe_obj_t* this);
+
+  pipe_t* pipe;
+  // PIPE_READER or PIPE_WRITER
+  int which_end;
+} pipe_obj;
+
+// Allocate new fd_objs with the pipe inside
+void pipe_obj_new(fd_obj* res[2]);
 
 pipe_t* pipe_new(size_t cap);
 void pipe_free(pipe_t* pipe);
